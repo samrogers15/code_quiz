3b6c84125dcd9492ef14d42205c5cae80a890f80
Sam Rogers <samrogers15@gmail.com>
2020-11-25 12:59:36 -0800

    Comitting additional round of changes to repository

---
+++ script.js
@@ -0,0 +1,18 @@
+var startButton = document.getElementById('start_button');
+
+
+// add event listener to start button so that when it is clicked, a function performs 
+
+// make questions a variable and make the answers an array (make an object with)
+
+var askQuestionOne = {
+    questionOne: 'how old are you?',
+    answerA: 23,
+    answerB: 24,
+    answerC: 25,
+    answerD: 26,
+}
+console.log(askQuestionOne)
+// if else statements
+
+// change text content of element to be question variable and answer array
